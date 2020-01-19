2bac8eeaad7dfce1c4bb47c3b576867ce61b9b3d
Logan Barnes <barneslt63@gmail.com>
2020-01-19 16:07:31 +0000

    Fix format files

--- format.sh
+++ format.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
-FILE_LIST="$(find src -type f -name '*.cpp' -o -name '*.hpp' -o -name '*.cuh' -o -name '*.cu')"
-clang-format-8 -i -style=file $FILE_LIST
+FILE_LIST="$(find . -type f -name '*.cpp' -o -name '*.hpp' -o -name '*.cu' -o -name '*.cuh' | grep -v cmake-build)"
+#echo "Formatting: ${FILE_LIST}"
+# shellcheck disable=SC2086
+clang-format-8 -i -style=file ${FILE_LIST}
