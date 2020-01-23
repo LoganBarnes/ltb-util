729574dafab0932cecea9d3b58cc9dc5bdeb418f
Logan Barnes <logan@cloudnc.com>
2020-01-23 15:23:59 +0000

    Gather cells and compute internal and external distances

--- src/ltb/util/comparison_utils.hpp
+++ src/ltb/util/comparison_utils.hpp
@@ -22,11 +22,28 @@
 // ///////////////////////////////////////////////////////////////////////////////////////
 #pragma once
 
+#define GOOGLE_COMPARISON
+
+#ifdef GOOGLE_COMPARISON
+#include <google/AlmostEquals.h>
+#else
 #include <cmath>
 #include <limits>
+#endif
+
+// standard
 #include <type_traits>
 
 namespace ltb::util {
+
+#ifdef GOOGLE_COMPARISON
+
+template <typename T>
+auto constexpr almost_equal(T a, T b) -> typename std::enable_if_t<std::is_floating_point_v<T>, bool> {
+    return AlmostEquals(a, b);
+}
+
+#else
 
 template <typename T>
 auto constexpr almost_equal(T a, T b, T scale = 1.0, T epsilon = 10 * std::numeric_limits<T>::epsilon()) ->
@@ -35,5 +52,7 @@ auto constexpr almost_equal(T a, T b, T scale = 1.0, T epsilon = 10 * std::numer
     const bool exactly_equal = (a == b);
     return exactly_equal || std::abs(a - b) < epsilon * (scale + std::abs(a) + std::abs(b));
 }
+
+#endif
 
 } // namespace ltb::util
