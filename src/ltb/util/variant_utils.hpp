81c18aa779cd39f8333c8480f637622240c85b15
Logan Barnes <barneslt63@gmail.com>
2020-03-06 10:26:14 +0000

    Add printing

--- src/ltb/util/variant_utils.hpp
+++ src/ltb/util/variant_utils.hpp
@@ -31,13 +31,13 @@ namespace ltb::util {
  * Wrapper around std::visit that forces the second parameter to be a std::variant class. This works
  * with objects implicitly convertible into variants (like classes deriving from std::variant).
  */
-template <typename Visitor, typename... Variants>
-constexpr decltype(auto) visit(Visitor&& visitor, std::variant<Variants...>& variants) {
+template <typename Visitor, typename... Ts>
+constexpr auto visit(Visitor&& visitor, std::variant<Ts...>& variants) {
     return std::visit(std::forward<Visitor>(visitor), variants);
 }
 
-template <typename Visitor, typename... Variants>
-constexpr decltype(auto) visit(Visitor&& visitor, const std::variant<Variants...>& variants) {
+template <typename Visitor, typename... Ts>
+constexpr auto visit(Visitor&& visitor, std::variant<Ts...> const& variants) {
     return std::visit(std::forward<Visitor>(visitor), variants);
 }
 
