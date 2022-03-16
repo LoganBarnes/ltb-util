d8f423fee3f528c73527702b03f218054c23c087
Logan Barnes <barneslt63@gmail.com>
2022-03-16 18:09:02 +0000

    Update ltb-target functions

--- cmake/LtbAddTarget.cmake
+++ cmake/LtbAddTarget.cmake
@@ -3,25 +3,74 @@
 ##########################################################################################
 
 function(ltb_add_executable target)
-    add_executable(${target} ${ARGN})
-    target_link_libraries(${target} PRIVATE doctest::doctest)
-    target_compile_definitions(${target} PRIVATE DOCTEST_CONFIG_DISABLE)
-    if (${LTB_ENABLE_TESTING})
-        add_executable(test-${target} ${ARGN})
-        target_link_libraries(test-${target} PRIVATE doctest::doctest)
+    add_executable(
+            ${target}
+            ${ARGN}
+    )
+    target_link_libraries(
+            ${target}
+            PRIVATE
+            doctest::doctest
+            $<TARGET_NAME_IF_EXISTS:ltb-dev-settings>
+    )
+    target_compile_definitions(
+            ${target}
+            PRIVATE
+            DOCTEST_CONFIG_DISABLE
+    )
+
+    if (${BUILD_TESTING})
+        add_executable(
+                test-${target}
+                EXCLUDE_FROM_ALL
+                ${ARGN}
+        )
+        target_link_libraries(
+                test-${target}
+                PRIVATE
+                doctest::doctest
+                $<TARGET_NAME_IF_EXISTS:ltb-dev-settings>
+        )
+        add_test(
+                NAME test-${target}
+                COMMAND $<TARGET_FILE:test-${target}>
+        )
     endif ()
 endfunction()
 
 function(ltb_add_library target)
-    add_library(${target} ${ARGN})
-    target_link_libraries(${target} PRIVATE doctest::doctest)
-    target_compile_definitions(${target} PRIVATE DOCTEST_CONFIG_DISABLE)
-    if (${LTB_ENABLE_TESTING})
-        add_executable(test-${target} ${ARGN} ${LTB_TEST_MAIN})
-        target_link_libraries(test-${target} PRIVATE
-                              $<$<BOOL:${LTB_TEST_MAIN}>:doctest::doctest>
-                              $<$<NOT:$<BOOL:${LTB_TEST_MAIN}>>:doctest_with_main>
-                              )
+    add_library(
+            ${target}
+            ${ARGN}
+    )
+    target_link_libraries(
+            ${target}
+            PRIVATE
+            doctest::doctest
+            $<TARGET_NAME_IF_EXISTS:ltb-dev-settings>
+    )
+    target_compile_definitions(
+            ${target}
+            PRIVATE DOCTEST_CONFIG_DISABLE
+    )
+
+    if (${BUILD_TESTING})
+        add_executable(
+                test-${target}
+                ${ARGN}
+                ${LTB_TEST_MAIN}
+        )
+        target_link_libraries(
+                test-${target}
+                PRIVATE
+                $<$<BOOL:${LTB_TEST_MAIN}>:doctest::doctest>
+                $<$<NOT:$<BOOL:${LTB_TEST_MAIN}>>:doctest_with_main>
+                $<TARGET_NAME_IF_EXISTS:ltb-dev-settings>
+        )
+        add_test(
+                NAME test-${target}
+                COMMAND $<TARGET_FILE:test-${target}>
+        )
     endif ()
 endfunction()
 
